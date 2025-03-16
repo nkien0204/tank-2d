691a846d72eb53a090cd912da2c8d8c8cbf9dcb4
nkien0204 <nkien0204@gmail.com>
2025-03-16 16:47:35 +0700

    update opponent movement

--- src/plugins/opponent.rs
+++ src/plugins/opponent.rs
@@ -41,11 +41,6 @@ fn spawn_opponent(
 
     let mut rng = rand::rng();
 
-    let translation = Vec3::new(
-        rng.random_range(SPAWN_RANGE_X.clone()),
-        rng.random_range(SPAWN_RANGE_Y.clone()),
-        0.0,
-    );
     let mut random_unit_vector = || {
         Vec3::new(
             rng.random_range(-1.0..1.0),
@@ -57,6 +52,24 @@ fn spawn_opponent(
     let velocity = random_unit_vector() * super::VELOCITY_SCALAR;
     let acceleration = random_unit_vector() * super::ACCELERATION_SCALAR;
 
+    let translation = Vec3::new(
+        rng.random_range(SPAWN_RANGE_X.clone()),
+        rng.random_range(SPAWN_RANGE_Y.clone()),
+        0.0,
+    );
+    let mut transform = Transform {
+        translation,
+        scale: super::DEFAULT_SCALE,
+        ..default()
+    };
+
+    let rotate_angle = calculate_angle(Vec3::new(0.0, 1.0, 0.0), velocity);
+    if velocity.x < 0.0 {
+        transform.rotate_z(rotate_angle);
+    } else {
+        transform.rotate_z(-rotate_angle);
+    }
+
     commands.spawn((
         MovingObjectBundle {
             velocity: Velocity { value: velocity },
@@ -67,11 +80,7 @@ fn spawn_opponent(
                 radius: OPPONENT_RADIUS,
                 colliding_entities: Vec::new(),
             },
-            transform: Transform {
-                translation,
-                scale: super::DEFAULT_SCALE,
-                ..default()
-            },
+            transform,
             model: Sprite {
                 image: image_assets.opponent.clone(),
                 ..default()
@@ -94,4 +103,17 @@ fn handle_opponent_collision(
             commands.entity(entity).despawn_recursive();
         }
     }
+}
+
+fn calculate_angle(u: Vec3, v: Vec3) -> f32 {
+    let dot_product = u.dot(v);
+    let magnitude_u = u.length();
+    let magnitude_v = v.length();
+
+    if magnitude_u == 0.0 || magnitude_v == 0.0 {
+        return 0.0;
+    }
+
+    let cos_theta = (dot_product / (magnitude_u * magnitude_v)).clamp(-1.0, 1.0);
+    cos_theta.acos() // This returns the angle in radians
 }
